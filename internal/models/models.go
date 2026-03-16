21a0eea332d8e69f5a4ff7ecaae3b31d8f51d1df
Kerem451I <luciferms471@gmail.com>
2026-03-16 22:18:48 +0300

    feat: add Endpoint and Check models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,23 @@
 package models
+
+import "time"
+
+type Endpoint struct {
+	ID              int64
+	Name            string
+	URL             string
+	IntervalSeconds int
+	ExpectedStatus  int
+	IsActive        bool
+	CreatedAt       time.Time
+}
+
+type Check struct {
+	ID         int64
+	EndpointID int64
+	CheckedAt  time.Time
+	Succeeded  bool
+	StatusCode *int
+	LatencyMs  *int
+	ErrorMsg   *string
+}
